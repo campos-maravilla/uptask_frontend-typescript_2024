14c5c4a62432c8fa5c39b0128f2739e307501a9f
campos-maravilla <antonycampos25@outlook.com>
2024-04-25 10:15:59 -0600

    creando-la-pagina-para-crear-proyectos,se crea la carpeta de proyects y se crea el archivo de CreateProjectNew.tsx

--- src/router.tsx
+++ src/router.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter,Routes,Route } from "react-router-dom";
 import AppLayout from "@/layouts/AppLayout";
 import DashboardViews from "@/views/DashboardViews";
+import CreateProjectView from "./views/proyects/CreateProjectView";
 
 export default function Router(){
     return (
@@ -8,6 +9,7 @@ export default function Router(){
     <Routes>
         <Route element={<AppLayout />}>
           <Route path="/" element={<DashboardViews/>} index />
+          <Route path="/projects/create" element={<CreateProjectView/>} />
         </Route>
     </Routes>
 </BrowserRouter>
