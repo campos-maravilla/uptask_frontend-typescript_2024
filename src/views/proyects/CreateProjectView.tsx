dd7f87732834823bae2d301ac847a4ccf5c18d6f
campos-maravilla <antonycampos25@outlook.com>
2024-04-25 21:40:09 -0600

    agregando-el-resto-de-campos,se crea el componente ErrorMesage.tsx y una carpeta dentro de components llamada projects y ahi se crea elarchivo ProjectForm.tsx y este se llama en CreateProjectView.tsx y se le pasa el props de register y errors

--- src/views/proyects/CreateProjectView.tsx
+++ src/views/proyects/CreateProjectView.tsx
@@ -1,3 +1,4 @@
+import ProjectForm from "@/components/projects/ProjectForm";
 import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
 
@@ -33,6 +34,10 @@ export default function CreateProjectView() {
         onSubmit={handleSubmit(handleForm)}
         noValidate
         >
+          <ProjectForm
+          register={register}
+          errors={errors}
+           />
          <input type="submit"
          value='Crear Proyecto'
          className="bg-fuchsia-600 hover:bg-fuchsia-700 w-full p-3 text-white uppercase font-bold cursor-pointer transition-colors"
