f5f662fa4368708c0d05dfd593942f4f73583075
campos-maravilla <antonycampos25@outlook.com>
2024-04-30 20:33:34 -0600

    obtener-el-proyecto-a-editar

--- src/api/ProjectAPI.ts
+++ src/api/ProjectAPI.ts
@@ -1,5 +1,5 @@
 import api from "@/lib/axios";
-import { ProjectFormData, dashboardProjectSchema } from "../types";
+import { Project, ProjectFormData, dashboardProjectSchema } from "../types";
 import { isAxiosError } from "axios";
 
 export async function createProject(formData:ProjectFormData){
@@ -23,6 +23,17 @@ export async function getProjects(){
        if(response.success){
         return response.data
        }
+    } catch (error) {
+        if(isAxiosError(error) && error.response){
+            throw new Error(error.response.data.error)
+        }
+    }
+}
+
+export async function getProjectById(id:Project['_id']){
+    try {
+        const {data}=await api(`/projects/${id}`)
+       return data
     } catch (error) {
         if(isAxiosError(error) && error.response){
             throw new Error(error.response.data.error)
--- src/views/proyects/EditProjectView.tsx
+++ src/views/proyects/EditProjectView.tsx
@@ -1,12 +1,17 @@
 import { useParams } from "react-router-dom"
-
-
+import { useQuery } from "@tanstack/react-query"
+import { getProjectById } from "@/api/ProjectAPI"
 
 export default function EditProjectView() {
     const params=useParams()
     const projectId=params.projectId! // ese valor siempre va a existir 
-    console.log(projectId)
-    
+   // console.log(projectId)
+   const {data,isLoading,error,isError}= useQuery({
+    queryKey:['editProjects',projectId],
+    queryFn:()=>getProjectById(projectId),
+   retry:false
+  })
+    console.log(data)
   return (
     <div>EditProjectView</div>
   )
