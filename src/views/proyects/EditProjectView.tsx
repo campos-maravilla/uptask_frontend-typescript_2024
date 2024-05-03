e116a88b3c454fa0f8f6400d2840215cd38d2352
campos-maravilla <antonycampos25@outlook.com>
2024-05-02 20:58:27 -0600

    creando-el-formulario-para-editar-proyectos

--- src/views/proyects/EditProjectView.tsx
+++ src/views/proyects/EditProjectView.tsx
@@ -1,18 +1,20 @@
-import { useParams } from "react-router-dom"
+import { Navigate, useParams } from "react-router-dom"
 import { useQuery } from "@tanstack/react-query"
 import { getProjectById } from "@/api/ProjectAPI"
+import EditProjectsForm from "@/components/projects/EditProjectsForm"
 
 export default function EditProjectView() {
     const params=useParams()
     const projectId=params.projectId! // ese valor siempre va a existir 
    // console.log(projectId)
-   const {data,isLoading,error,isError}= useQuery({
+   const {data,isLoading,isError}= useQuery({
     queryKey:['editProjects',projectId],
     queryFn:()=>getProjectById(projectId),
    retry:false
   })
-    console.log(data)
-  return (
-    <div>EditProjectView</div>
-  )
+    
+  if(isLoading) return 'Cargando...'
+  if(isError) return <Navigate to='/404' />
+ 
+  if(data) return <EditProjectsForm />
 }
