b5618bcc53dba984de323e8e253a0cfc0e047ae1
campos-maravilla <antonycampos25@outlook.com>
2024-05-30 16:25:18 -0600

    creando-la-vista-para-registro-y-routing-entre-las-dos-vistas

--- src/router.tsx
+++ src/router.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter,Routes,Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AppLayout from "@/layouts/AppLayout";
 import DashboardViews from "@/views/DashboardViews";
 import CreateProjectView from "./views/proyects/CreateProjectView";
@@ -6,23 +6,27 @@ import EditProjectView from "./views/proyects/EditProjectView";
 import ProjectDetailsView from "./components/projects/ProjectDetailsView";
 import AuthLayout from "./layouts/AuthLayout";
 import LoginView from "./views/auth/LoginView";
+import RegisterView from "./views/auth/RegisterView";
 
-export default function Router(){
-    return (
-<BrowserRouter>
-    <Routes>
+export default function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
         <Route element={<AppLayout />}>
-          <Route path="/" element={<DashboardViews/>} index />
-          <Route path="/projects/create" element={<CreateProjectView/>} />
-          <Route path="/projects/:projectId/edit" element={<EditProjectView/>} />
-          <Route path="/projects/:projectId" element={<ProjectDetailsView/>} />
+          <Route path="/" element={<DashboardViews />} index />
+          <Route path="/projects/create" element={<CreateProjectView />} />
+          <Route
+            path="/projects/:projectId/edit"
+            element={<EditProjectView />}
+          />
+          <Route path="/projects/:projectId" element={<ProjectDetailsView />} />
         </Route>
-        
-        <Route element={<AuthLayout/>}>
-            <Route path='/auth/login' element={<LoginView/>} />
-            
+
+        <Route element={<AuthLayout />}>
+          <Route path="/auth/login" element={<LoginView />} />
+          <Route path="/auth/register" element={<RegisterView />} />
         </Route>
-    </Routes>
-</BrowserRouter>
-    )
+      </Routes>
+    </BrowserRouter>
+  );
 }
--- src/types/index.ts
+++ src/types/index.ts
@@ -11,6 +11,8 @@ const authSchema = z.object({
 
 type Auth = z.infer<typeof authSchema>
 export type UserLoginForm = Pick<Auth, 'email' | 'password'>
+export type UserRegistrationForm = Pick<Auth, 'name' | 'email' | 'password' | 'password_confirmation'>
+
 /** Tasks **/
 
 export const taskStatusSchema = z.enum(['pending', 'onHold', 'inProgress', 'underReview', 'completed'])
