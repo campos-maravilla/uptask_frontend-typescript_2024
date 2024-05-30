a3d7030bc3311b1b80c7faf59e85a37bef2e7a77
campos-maravilla <antonycampos25@outlook.com>
2024-05-30 16:07:34 -0600

    creando-la-vista-de-login-y-schema-de-zod-para-el-formulario

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,39 +1,50 @@
 import { z } from "zod";
 
-/* Tasks */
 
-export const taskStatusSchema=z.enum(['pending', 'onHold', 'inProgress', 'underReview','completed'])
-export type TaskStatus=z.infer<typeof taskStatusSchema>
-
-export const taskSchema=z.object({
-    _id:z.string(),
-    name:z.string(),
-    description:z.string(),
-    project:z.string(),
-    status:taskStatusSchema,
-    createdAt:z.string(),
-    updatedAt:z.string()
+/** Auth & Users **/
+const authSchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string(),
+    password_confirmation: z.string(),
 })
 
-export type Task=z.infer<typeof taskSchema>
-export type TaskFormData=Pick<Task,'name'|'description'>
+type Auth = z.infer<typeof authSchema>
+export type UserLoginForm = Pick<Auth, 'email' | 'password'>
+/** Tasks **/
+
+export const taskStatusSchema = z.enum(['pending', 'onHold', 'inProgress', 'underReview', 'completed'])
+export type TaskStatus = z.infer<typeof taskStatusSchema>
+
+export const taskSchema = z.object({
+    _id: z.string(),
+    name: z.string(),
+    description: z.string(),
+    project: z.string(),
+    status: taskStatusSchema,
+    createdAt: z.string(),
+    updatedAt: z.string()
+})
+
+export type Task = z.infer<typeof taskSchema>
+export type TaskFormData = Pick<Task, 'name' | 'description'>
 
 /* Projects */
-export const projectSchema=z.object({
-    _id:z.string(),
-    projectName:z.string(),
-    clientName:z.string(),
-    description:z.string()
+export const projectSchema = z.object({
+    _id: z.string(),
+    projectName: z.string(),
+    clientName: z.string(),
+    description: z.string()
 })
 
-export const dashboardProjectSchema=z.array(
+export const dashboardProjectSchema = z.array(
     projectSchema.pick({
-        _id:true,
-        projectName:true,
-        clientName:true,
-        description:true
+        _id: true,
+        projectName: true,
+        clientName: true,
+        description: true
     })
 )
 
-export type Project=z.infer<typeof projectSchema>
-export type ProjectFormData=Pick<Project,'clientName' | 'projectName' | 'description'>
+export type Project = z.infer<typeof projectSchema>
+export type ProjectFormData = Pick<Project, 'clientName' | 'projectName' | 'description'>
