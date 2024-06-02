53683a3c7fa09b42a593748959b8bd5f209f66b2
campos-maravilla <antonycampos25@outlook.com>
2024-06-02 08:06:28 -0600

    enviando-el-nuevo-codigo-de-confirmacion                                               ,se hace el endpoint de request-code,para que el usuario pueda solicitar un nuevo token,se hace en RequestNewCodeView.tsx

--- src/api/AuthAPI.ts
+++ src/api/AuthAPI.ts
@@ -1,7 +1,7 @@
 
 import api from "@/lib/axios";
 import { isAxiosError } from "axios";
-import { ConfirmToken, UserRegistrationForm } from "../types";
+import { ConfirmToken, RequestConfirmationCodeForm, UserRegistrationForm } from "../types";
 
 export async function createAccount(formData: UserRegistrationForm) {
     try {
@@ -19,6 +19,18 @@ export async function createAccount(formData: UserRegistrationForm) {
 export async function confirmAccount(formData: ConfirmToken) {
     try {
         const url = '/auth/confirm-account'
+        const { data } = await api.post<string>(url, formData)
+        return data
+    } catch (error) {
+        if (isAxiosError(error) && error.response) {
+            throw new Error(error.response.data.error)
+        }
+    }
+}
+//para solicitar nuevo token,en caso de que
+export async function requestConfirmationCode(formData: RequestConfirmationCodeForm) {
+    try {
+        const url = '/auth/request-code'
         const { data } = await api.post<string>(url, formData)
         return data
     } catch (error) {
