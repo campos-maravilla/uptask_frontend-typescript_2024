dfabb140c697ab2149ca49ab728fae1e62f04d08
campos-maravilla <antonycampos25@outlook.com>
2024-06-04 07:40:43 -0600

    validando-el-token-en-el-frontend-con-el-endpoint-de-validate-token

--- src/views/auth/NewPasswordView.tsx
+++ src/views/auth/NewPasswordView.tsx
@@ -1,8 +1,10 @@
 import NewPasswordToken from "@/components/auth/NewPasswordToken";
 import NewPasswordForm from "@/components/auth/NewPasswordForm";
 import React, { useState } from "react";
+import { ConfirmToken } from "@/types/index";
 
 export default function NewPasswordView() {
+  const [token, setToken] = useState<ConfirmToken["token"]>("");
   const [isValidToken, setIsValidToken] = useState(false);
 
   return (
@@ -13,7 +15,15 @@ export default function NewPasswordView() {
         <span className=" text-fuchsia-500 font-bold">por email</span>
       </p>
 
-      {!isValidToken ? <NewPasswordToken /> : <NewPasswordForm />}
+      {!isValidToken ? (
+        <NewPasswordToken
+          token={token}
+          setToken={setToken}
+          setIsValidToken={setIsValidToken}
+        />
+      ) : (
+        <NewPasswordForm />
+      )}
     </>
   );
 }
