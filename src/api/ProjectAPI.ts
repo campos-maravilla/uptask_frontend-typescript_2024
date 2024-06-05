a77b0da80d911754539fc4847d29c0034187c952
campos-maravilla <antonycampos25@outlook.com>
2024-06-05 15:32:15 -0600

    obtener-los-proyectos-desde-react-usando-el-token-con-localstorage,se pone en la url de projects el headers con Auhorization ,el bearer y el token,pero primero se guarda en locastorage con getItem

--- src/api/ProjectAPI.ts
+++ src/api/ProjectAPI.ts
@@ -2,67 +2,73 @@ import api from "@/lib/axios";
 import { Project, ProjectFormData, dashboardProjectSchema } from "../types";
 import { isAxiosError } from "axios";
 
-export async function createProject(formData:ProjectFormData){
+export async function createProject(formData: ProjectFormData) {
     try {
-        const {data}=await api.post('/projects',formData)
+        const { data } = await api.post('/projects', formData)
         //console.log(data)
         return data
     } catch (error) {
-        if(isAxiosError(error) && error.response){
+        if (isAxiosError(error) && error.response) {
             throw new Error(error.response.data.error)
         }
     }
 }
 
 //obtener todos los proyectos
-export async function getProjects(){
+export async function getProjects() {
+    const token = localStorage.getItem('AUTH_TOKEN')
+    console.log(token)
     try {
-        const {data}=await api('/projects')
-       //console.log(data)
-       const response=dashboardProjectSchema.safeParse(data)
-       if(response.success){
-        return response.data
-       }
+        const { data } = await api('/projects', {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        })
+        //console.log(data)
+        const response = dashboardProjectSchema.safeParse(data)
+        if (response.success) {
+            return response.data
+        }
     } catch (error) {
-        if(isAxiosError(error) && error.response){
+        if (isAxiosError(error) && error.response) {
             throw new Error(error.response.data.error)
         }
     }
 }
 
-export async function getProjectById(id:Project['_id']){
+export async function getProjectById(id: Project['_id']) {
     try {
-        const {data}=await api(`/projects/${id}`)
-       return data
+        const { data } = await api(`/projects/${id}`)
+        return data
     } catch (error) {
-        if(isAxiosError(error) && error.response){
+        if (isAxiosError(error) && error.response) {
             throw new Error(error.response.data.error)
         }
     }
 }
 
-type ProjectAPIType={
-    formData:ProjectFormData
-    projectId:Project['_id']
+type ProjectAPIType = {
+    formData: ProjectFormData
+    projectId: Project['_id']
 }
-export async function updateProject({formData,projectId}:ProjectAPIType){
+export async function updateProject({ formData, projectId }: ProjectAPIType) {
     try {
-        const {data}=await api.put<string>(`/projects/${projectId}`,formData)
-       return data
+        const { data } = await api.put<string>(`/projects/${projectId}`, formData)
+        return data
     } catch (error) {
-        if(isAxiosError(error) && error.response){
+        if (isAxiosError(error) && error.response) {
             throw new Error(error.response.data.error)
         }
     }
 }
 
-export async function deleteProject(id:Project['_id']){
+export async function deleteProject(id: Project['_id']) {
     try {
-        const url=`/projects/${id}`
-        const {data}=await api.delete<string>(url) 
-       return data
+        const url = `/projects/${id}`
+        const { data } = await api.delete<string>(url)
+        return data
     } catch (error) {
-        if(isAxiosError(error) && error.response){
+        if (isAxiosError(error) && error.response) {
             throw new Error(error.response.data.error)
         }
     }
