7368ae79aae5bdd6bb4c29c71fd8acb9a7d95206
campos-maravilla <antonycampos25@outlook.com>
2024-06-07 07:34:35 -0600

    creando-rutas-para-colaboradores,se crea una vista en la carpeta de projects en view con el nombre de ProjectTeamView.tsx

--- src/components/projects/ProjectDetailsView.tsx
+++ src/components/projects/ProjectDetailsView.tsx
@@ -1,49 +1,54 @@
-
-
-
-import { Navigate, useNavigate, useParams } from "react-router-dom"
-import { useQuery } from "@tanstack/react-query"
-import { getProjectById } from "@/api/ProjectAPI"
-import AddTaskModal from "../tasks/AddTaskModal"
-import TaskList from "../tasks/TaskList"
-import EditTaskData from "../tasks/EditTaskData"
-import TaskModalDetails from "../tasks/TaskModalDetails"
-
+import { Link, Navigate, useNavigate, useParams } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import { getProjectById } from "@/api/ProjectAPI";
+import AddTaskModal from "../tasks/AddTaskModal";
+import TaskList from "../tasks/TaskList";
+import EditTaskData from "../tasks/EditTaskData";
+import TaskModalDetails from "../tasks/TaskModalDetails";
 
 export default function ProjectDetailsView() {
-  
-  const navigate=useNavigate()
-  
-    const params=useParams()
-    const projectId=params.projectId! // ese valor siempre va a existir 
-   // console.log(projectId)
-   const {data,isLoading,isError}= useQuery({
-    queryKey:['project',projectId],
-    queryFn:()=>getProjectById(projectId),
-   retry:false
-  })
-    
-  if(isLoading) return 'Cargando...'
-  if(isError) return <Navigate to='/404' />
- 
-  if(data) return (
-    <>
-    <h1 className="text-5xl font-black">{data.projectName}</h1>
-    <p className="text-2xl font-light text-gray-500 mt-5">{data.description}</p>
-    
-    <nav className="my-5 flex gap-3">
-      <button
-      type="button"
-      className="bg-purple-400 hover:bg-purple-500 px-10 py-3 text-white text-xl font-bold cursor-pointer transition-colors"
-      onClick={()=>navigate(location.pathname+`?newTask=true`)}
-      >Agregar Tarea</button>
-    </nav>
-    
-    <TaskList tasks={data.tasks}/>
-    <AddTaskModal/>
-    <EditTaskData/>
-    <TaskModalDetails/>
-    </>
-  )
-}
+  const navigate = useNavigate();
 
+  const params = useParams();
+  const projectId = params.projectId!; // ese valor siempre va a existir
+  // console.log(projectId)
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ["project", projectId],
+    queryFn: () => getProjectById(projectId),
+    retry: false,
+  });
+
+  if (isLoading) return "Cargando...";
+  if (isError) return <Navigate to="/404" />;
+
+  if (data)
+    return (
+      <>
+        <h1 className="text-5xl font-black">{data.projectName}</h1>
+        <p className="text-2xl font-light text-gray-500 mt-5">
+          {data.description}
+        </p>
+
+        <nav className="my-5 flex gap-3">
+          <button
+            type="button"
+            className="bg-purple-600 hover:bg-purple-500 px-10 py-3 text-white text-xl font-bold cursor-pointer transition-colors"
+            onClick={() => navigate(location.pathname + `?newTask=true`)}
+          >
+            Agregar Tarea
+          </button>
+          <Link
+            to={"team"}
+            className="bg-fuchsia-400 hover:bg-purple-500 px-10 py-3 text-white text-xl font-bold cursor-pointer transition-colors"
+          >
+            Colaboradores
+          </Link>
+        </nav>
+
+        <TaskList tasks={data.tasks} />
+        <AddTaskModal />
+        <EditTaskData />
+        <TaskModalDetails />
+      </>
+    );
+}
