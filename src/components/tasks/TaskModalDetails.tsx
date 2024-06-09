ab9402f31e69bc464964cdedcfc5070038e30df0
campos-maravilla <antonycampos25@outlook.com>
2024-06-09 09:04:42 -0600

    mostrando-quien-modifico-la-tarea-en-el-modal,al cambiar el estado aparece el nombre del usuario quien modifico dicha tarea

--- src/components/tasks/TaskModalDetails.tsx
+++ src/components/tasks/TaskModalDetails.tsx
@@ -1,117 +1,146 @@
-
-import { Fragment } from 'react';
-import { Dialog, Transition } from '@headlessui/react';
-import { Navigate, useLocation, useNavigate, useParams } from 'react-router-dom';
-import { useQuery,useMutation,useQueryClient } from '@tanstack/react-query';
-import { getTaskById, updateStatusTask } from '@/api/TaskAPI';
-import { toast } from 'react-toastify';
-import { formatDate } from '@/utils/utils';
-import { statusTranslations } from '@/locales/es';
-import { TaskStatus } from '@/types/index';
-
+import { Fragment } from "react";
+import { Dialog, Transition } from "@headlessui/react";
+import {
+  Navigate,
+  useLocation,
+  useNavigate,
+  useParams,
+} from "react-router-dom";
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+import { getTaskById, updateStatusTask } from "@/api/TaskAPI";
+import { toast } from "react-toastify";
+import { formatDate } from "@/utils/utils";
+import { statusTranslations } from "@/locales/es";
+import { TaskStatus } from "@/types/index";
 
 export default function TaskModalDetails() {
-    
-    const params=useParams()
-    const projectId=params.projectId! 
-    
-    const navigate=useNavigate()
-   // leyendo query string ?viewTask=${task._id}=url 
-   const location=useLocation()
-   const queryParams=new URLSearchParams(location.search)
-   const taskId=queryParams.get('viewTask')!
+  const params = useParams();
+  const projectId = params.projectId!;
+
+  const navigate = useNavigate();
+  // leyendo query string ?viewTask=${task._id}=url
+  const location = useLocation();
+  const queryParams = new URLSearchParams(location.search);
+  const taskId = queryParams.get("viewTask")!;
   // console.log(taskId)
   // console.log(location.search)
-  
-  const show=taskId ? true : false 
-  
-  const {data,isError,error}=useQuery({
-    queryKey:['task',taskId],
-    queryFn:()=>getTaskById({projectId,taskId}),
-    enabled:!!taskId, //solamente si taskId existe,queremos mostrar esta consulta 
-    retry:false
-})
 
-const queryClient=useQueryClient()  
-const {mutate}=useMutation({
-    mutationFn:updateStatusTask,
-    onError:(error)=>{
-        toast.error(error.message)
+  const show = taskId ? true : false;
+
+  const { data, isError, error } = useQuery({
+    queryKey: ["task", taskId],
+    queryFn: () => getTaskById({ projectId, taskId }),
+    enabled: !!taskId, //solamente si taskId existe,queremos mostrar esta consulta
+    retry: false,
+  });
+
+  const queryClient = useQueryClient();
+  const { mutate } = useMutation({
+    mutationFn: updateStatusTask,
+    onError: (error) => {
+      toast.error(error.message);
     },
-    onSuccess:(data)=>{
-        toast.success(data)
-        queryClient.invalidateQueries({queryKey:['project',projectId]}) 
-        queryClient.invalidateQueries({queryKey:['task',taskId]}) 
-    }
-})
+    onSuccess: (data) => {
+      toast.success(data);
+      queryClient.invalidateQueries({ queryKey: ["project", projectId] });
+      queryClient.invalidateQueries({ queryKey: ["task", taskId] });
+    },
+  });
 
-const handleChange=(e:React.ChangeEvent<HTMLSelectElement>)=>{
-   // console.log(e.target.value)
-   const status=e.target.value as TaskStatus
-   const data={projectId,taskId,status}
-   //console.log(data)
-   mutate(data)
-}
+  const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    // console.log(e.target.value)
+    const status = e.target.value as TaskStatus;
+    const data = { projectId, taskId, status };
+    //console.log(data)
+    mutate(data);
+  };
 
-if(isError){
-    toast.error(error.message,{toastId:'error'})
-    return <Navigate to={`/projects/${projectId}`} />
-}
-  
- if(data)   return (
-        <>
-            <Transition appear show={show} as={Fragment}>
-                <Dialog as="div" className="relative z-10" onClose={() => navigate(location.pathname,{replace:true})}> 
-                    <Transition.Child
-                        as={Fragment}
-                        enter="ease-out duration-300"
-                        enterFrom="opacity-0"
-                        enterTo="opacity-100"
-                        leave="ease-in duration-200"
-                        leaveFrom="opacity-100"
-                        leaveTo="opacity-0"
+  if (isError) {
+    toast.error(error.message, { toastId: "error" });
+    return <Navigate to={`/projects/${projectId}`} />;
+  }
+
+  if (data)
+    return (
+      <>
+        <Transition appear show={show} as={Fragment}>
+          <Dialog
+            as="div"
+            className="relative z-10"
+            onClose={() => navigate(location.pathname, { replace: true })}
+          >
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <div className="fixed inset-0 bg-black/60" />
+            </Transition.Child>
+
+            <div className="fixed inset-0 overflow-y-auto">
+              <div className="flex min-h-full items-center justify-center p-4 text-center">
+                <Transition.Child
+                  as={Fragment}
+                  enter="ease-out duration-300"
+                  enterFrom="opacity-0 scale-95"
+                  enterTo="opacity-100 scale-100"
+                  leave="ease-in duration-200"
+                  leaveFrom="opacity-100 scale-100"
+                  leaveTo="opacity-0 scale-95"
+                >
+                  <Dialog.Panel className="w-full max-w-4xl transform overflow-hidden rounded-2xl bg-white text-left align-middle shadow-xl transition-all p-16">
+                    <p className="text-sm text-slate-400">
+                      Agregada el: {formatDate(data.createdAt)}{" "}
+                    </p>
+                    <p className="text-sm text-slate-400">
+                      Última actualización: {formatDate(data.updatedAt)}
+                    </p>
+                    <Dialog.Title
+                      as="h3"
+                      className="font-black text-4xl text-slate-600 my-5"
                     >
-                        <div className="fixed inset-0 bg-black/60" />
-                    </Transition.Child>
+                      {data.name}
+                    </Dialog.Title>
+                    <p className="text-lg text-slate-500 mb-2">
+                      Descripción: {data.description}
+                    </p>
+                    {data.completedBy && (
+                      <p>
+                        <span className="font-bold text-slate-600">
+                          Estado actualizado por:
+                        </span>{" "}
+                        {data.completedBy.name}
+                      </p>
+                    )}
+                    <div className="my-5 space-y-3">
+                      <label className="font-bold">
+                        Estado Actual: {data.status}
+                      </label>
 
-                    <div className="fixed inset-0 overflow-y-auto">
-                        <div className="flex min-h-full items-center justify-center p-4 text-center">
-                            <Transition.Child
-                                as={Fragment}
-                                enter="ease-out duration-300"
-                                enterFrom="opacity-0 scale-95"
-                                enterTo="opacity-100 scale-100"
-                                leave="ease-in duration-200"
-                                leaveFrom="opacity-100 scale-100"
-                                leaveTo="opacity-0 scale-95"
-                            >
-                                <Dialog.Panel className="w-full max-w-4xl transform overflow-hidden rounded-2xl bg-white text-left align-middle shadow-xl transition-all p-16">
-                                    <p className='text-sm text-slate-400'>Agregada el: {formatDate(data.createdAt)} </p>
-                                    <p className='text-sm text-slate-400'>Última actualización: {formatDate(data.updatedAt)}</p>
-                                    <Dialog.Title
-                                        as="h3"
-                                        className="font-black text-4xl text-slate-600 my-5"
-                                    >{data.name}</Dialog.Title>
-                                    <p className='text-lg text-slate-500 mb-2'>Descripción: {data.description}</p>
-                                    <div className='my-5 space-y-3'>
-                                        <label className='font-bold'>Estado Actual:  {data.status}</label>
-                                        
-                                        <select 
-                                        className='w-full p-3 border border-gray-300'
-                                        defaultValue={data.status}
-                                        onChange={handleChange} 
-                                        >
-                                        {Object.entries(statusTranslations).map(([key,value])=>(
-                                            <option key={key} value={key}>{value}</option>
-                                        ))}   
-                                        </select>
-                                    </div>
-                                </Dialog.Panel>
-                            </Transition.Child>
-                        </div>
+                      <select
+                        className="w-full p-3 border border-gray-300"
+                        defaultValue={data.status}
+                        onChange={handleChange}
+                      >
+                        {Object.entries(statusTranslations).map(
+                          ([key, value]) => (
+                            <option key={key} value={key}>
+                              {value}
+                            </option>
+                          )
+                        )}
+                      </select>
                     </div>
-                </Dialog>
-            </Transition>
-        </>
-    )
+                  </Dialog.Panel>
+                </Transition.Child>
+              </div>
+            </div>
+          </Dialog>
+        </Transition>
+      </>
+    );
 }
