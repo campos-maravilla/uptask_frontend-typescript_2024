bd533db3604466e45a729a2aad7ef37ea9b969f0
campos-maravilla <antonycampos25@outlook.com>
2024-06-11 10:30:58 -0600

    modificando-AddTaskModal-EditTaskModal-EditProjectView-y-ProjectDetailsView,esto se hace porque al guardar una tarea no la mostraba ,solo al regargar

--- src/views/proyects/EditProjectView.tsx
+++ src/views/proyects/EditProjectView.tsx
@@ -1,20 +1,20 @@
-import { Navigate, useParams } from "react-router-dom"
-import { useQuery } from "@tanstack/react-query"
-import { getProjectById } from "@/api/ProjectAPI"
-import EditProjectsForm from "@/components/projects/EditProjectsForm"
+import { Navigate, useParams } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import { getProjectById } from "@/api/ProjectAPI";
+import EditProjectsForm from "@/components/projects/EditProjectsForm";
 
 export default function EditProjectView() {
-    const params=useParams()
-    const projectId=params.projectId! // ese valor siempre va a existir 
-   // console.log(projectId)
-   const {data,isLoading,isError}= useQuery({
-    queryKey:['editProjects',projectId],
-    queryFn:()=>getProjectById(projectId),
-   retry:false
-  })
-    
-  if(isLoading) return 'Cargando...'
-  if(isError) return <Navigate to='/404' />
- 
-  if(data) return <EditProjectsForm data={data} projectId={projectId} />
+  const params = useParams();
+  const projectId = params.projectId!; // ese valor siempre va a existir
+  // console.log(projectId)
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ["editProject", projectId],
+    queryFn: () => getProjectById(projectId),
+    retry: false,
+  });
+
+  if (isLoading) return "Cargando...";
+  if (isError) return <Navigate to="/404" />;
+
+  if (data) return <EditProjectsForm data={data} projectId={projectId} />;
 }
