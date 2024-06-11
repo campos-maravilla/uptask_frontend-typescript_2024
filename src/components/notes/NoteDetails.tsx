faa2fc03a86057e32bb8441b066d21156c669fd3
campos-maravilla <antonycampos25@outlook.com>
2024-06-11 12:36:49 -0600

    detectando-si-el-usuario-creo-la-nota-para-que-pueda-eliminar,se crea el boton de Eliminar en NoteDetail.tsx

--- src/components/notes/NoteDetails.tsx
+++ src/components/notes/NoteDetails.tsx
@@ -1,13 +1,20 @@
+import { useAuth } from "@/hooks/useAuth";
 import { Note } from "@/types/index";
 import { formatDate } from "@/utils/utils";
+import { useMemo } from "react";
 
 type NoteDetailProps = {
   note: Note;
 };
 
 export default function NoteDetails({ note }: NoteDetailProps) {
+  const { data, isLoading } = useAuth();
+  const canDelete = useMemo(() => data?._id === note.createdBy._id, [data]);
+
+  if (isLoading) return "Cargando...";
+
   return (
-    <div className="p-3 justify-between items-center">
+    <div className="p-3 flex justify-between items-center">
       <div>
         <p>
           {note.content} por:{" "}
@@ -15,6 +22,11 @@ export default function NoteDetails({ note }: NoteDetailProps) {
         </p>
         <p className="text-xs text-slate-500">{formatDate(note.createdAt)}</p>
       </div>
+      {canDelete && (
+        <button className="bg-red-400 hover:bg-red-500 p-2 text-xs text-white font-bold cursor-pointer transition-colors">
+          Eliminar
+        </button>
+      )}
     </div>
   );
 }
