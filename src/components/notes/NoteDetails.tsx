23458e4572862aea2694e5464d9ffbe875a714e4
campos-maravilla <antonycampos25@outlook.com>
2024-06-12 07:20:17 -0600

    eliminando-las-notas-e-invalidando-los-queries,se usa { queryKey: [task, taskId] },para que elimine sin recargar

--- src/components/notes/NoteDetails.tsx
+++ src/components/notes/NoteDetails.tsx
@@ -1,7 +1,11 @@
+import { deleteNote } from "@/api/NoteAPI";
 import { useAuth } from "@/hooks/useAuth";
 import { Note } from "@/types/index";
 import { formatDate } from "@/utils/utils";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useMemo } from "react";
+import { useLocation, useParams } from "react-router-dom";
+import { toast } from "react-toastify";
 
 type NoteDetailProps = {
   note: Note;
@@ -10,6 +14,23 @@ type NoteDetailProps = {
 export default function NoteDetails({ note }: NoteDetailProps) {
   const { data, isLoading } = useAuth();
   const canDelete = useMemo(() => data?._id === note.createdBy._id, [data]);
+
+  const params = useParams();
+  const location = useLocation();
+  const queryParams = new URLSearchParams(location.search);
+  const taskId = queryParams.get("viewTask")!;
+
+  const projectId = params.projectId!;
+
+  const queryClient = useQueryClient();
+  const { mutate } = useMutation({
+    mutationFn: deleteNote,
+    onError: (error) => toast.error(error.message),
+    onSuccess: (data) => {
+      toast.success(data);
+      queryClient.invalidateQueries({ queryKey: ["task", taskId] });
+    },
+  });
 
   if (isLoading) return "Cargando...";
 
@@ -23,7 +44,10 @@ export default function NoteDetails({ note }: NoteDetailProps) {
         <p className="text-xs text-slate-500">{formatDate(note.createdAt)}</p>
       </div>
       {canDelete && (
-        <button className="bg-red-400 hover:bg-red-500 p-2 text-xs text-white font-bold cursor-pointer transition-colors">
+        <button
+          className="bg-red-400 hover:bg-red-500 p-2 text-xs text-white font-bold cursor-pointer transition-colors"
+          onClick={() => mutate({ projectId, taskId, noteId: note._id })}
+        >
           Eliminar
         </button>
       )}
