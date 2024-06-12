ef7c3edb910a68f8140cf0489a7903eb552f4e38
campos-maravilla <antonycampos25@outlook.com>
2024-06-12 12:27:16 -0600

    creando-el-formulario-de-perfil

--- src/views/profile/ProfileView.tsx
+++ src/views/profile/ProfileView.tsx
@@ -1,3 +1,11 @@
+import ProfileForm from "@/components/profile/ProfileForm";
+import { useAuth } from "@/hooks/useAuth";
+
 export default function ProfileView() {
-  return <div>ProfileView</div>;
+  //no se usa error porque ya lo tiene AppLayout,y si hay error lo manda a login
+  const { data, isLoading } = useAuth();
+
+  if (isLoading) return "Cargando...";
+
+  if (data) return <ProfileForm data={data} />;
 }
