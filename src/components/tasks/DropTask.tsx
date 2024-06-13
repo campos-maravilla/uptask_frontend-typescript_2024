2caa8950c2dfd984b260bbc8b1d7feb13dee55e9
campos-maravilla <antonycampos25@outlook.com>
2024-06-13 09:27:32 -0600

    primeros-pasos-con-dnd-kit-drag-and-drop

---
+++ src/components/tasks/DropTask.tsx
@@ -0,0 +1,8 @@
+export default function DropTask() {
+  return (
+    <div className="text-sm font-semibold uppercase p-2 border border-dashed border-slate-500 mt-5 place-content-center text-slate-500">
+      {" "}
+      Soltar Tarea aqui
+    </div>
+  );
+}
