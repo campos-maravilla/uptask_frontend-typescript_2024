15382ea599cbe7a355863c562b00d6febcddfe0c
campos-maravilla <antonycampos25@outlook.com>
2024-06-13 16:57:37 -0600

    añadiendo-un-efecto-de-drag

--- src/components/tasks/DropTask.tsx
+++ src/components/tasks/DropTask.tsx
@@ -8,8 +8,13 @@ export default function DropTask({ status }: DropTaskProps) {
   const { isOver, setNodeRef } = useDroppable({
     id: status,
   });
+
+  const style = {
+    opacity: isOver ? 0.4 : undefined,
+  };
   return (
     <div
+      style={style}
       ref={setNodeRef}
       className="text-sm font-semibold uppercase p-2 border border-dashed border-slate-500 mt-5 place-content-center text-slate-500"
     >
