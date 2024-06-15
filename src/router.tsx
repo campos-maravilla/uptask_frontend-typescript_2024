3708596241b80e85130da133aa5a08319e05510f
campos-maravilla <antonycampos25@outlook.com>
2024-06-15 13:13:26 -0600

    pagina-404-algo-no-existe

--- src/router.tsx
+++ src/router.tsx
@@ -15,6 +15,7 @@ import ProjectTeamView from "./views/proyects/ProjectTeamView";
 import ProfileView from "./views/profile/ProfileView";
 import ChangePasswordView from "./views/profile/ChangePasswordView";
 import ProfileLayout from "./layouts/ProfileLayout";
+import NotFound from "./views/404/NotFound";
 
 export default function Router() {
   return (
@@ -54,6 +55,9 @@ export default function Router() {
             element={<ForgotPasswordView />}
           />
           <Route path="/auth/new-password" element={<NewPasswordView />} />
+        </Route>
+        <Route element={<AuthLayout />}>
+          <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
     </BrowserRouter>
